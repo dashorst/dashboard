05f5f71b48034ae4d291bdee7b00d18786e3a28a
Martijn Dashorst <martijn.dashorst@gmail.com>
2010-09-20 12:45:07 +0200

    Ticket tape twitter feed

--- client/client.js
+++ client/client.js
@@ -1,8 +1,18 @@
-getTwitters('twitter', { 
-  id: 'dashorst', 
-  count: 1, 
-  enableLinks: true, 
-  ignoreReplies: true, 
-  clearContents: true,
-  template: '%time% %text%'
-});
+
+var myTweets = {
+  'delayedRefresh' : function() {
+    setTimeout(myTweets.refresh, 10 * 60 * 1000); // refresh twitters every 10 minutes
+  },
+  'refresh' : function() {
+	getTwitters('twitter', { 
+	  id: 'machielkarels', 
+	  count: 10, 
+	  enableLinks: true, 
+	  ignoreReplies: true, 
+	  clearContents: true,
+	  template: '%time% twitterde <a href="http://twitter.com/%user_screen_name%">@%user_screen_name%</a>: %text%',
+      callback: myTweets.delayedRefresh
+	});
+  }
+};
+myTweets.refresh();
