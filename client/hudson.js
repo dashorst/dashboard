a3eda98aee862c1e2852b19ea0ff52a6a2222d50
Martijn Dashorst <martijn.dashorst@gmail.com>
2010-09-21 09:40:44 +0200

    Hudson werkt nu, moet alleen nog de builds goed sorteren, nu komen ze random binnen

--- client/hudson.js
+++ client/hudson.js
@@ -1,19 +1,33 @@
 $(function () {
-	var project = getBuildsForProject("DUO");
+	getBuildsForProject("DUO");
+	getBuildsForProject("EduArte%20v2.03%20-%20trunk%20(ontwikkel)");
+	getBuildsForProject("Vocus%20-%20trunk%20(ontwikkel)");
+	getBuildsForProject("PassePartout");
 });
 
 function getBuildsForProject(name) {
+	var cleanedName = name.replace(/\(|\)|\.|\-|\%/g, '');
+	var id = 'hudson_' + cleanedName;
+	var markup = $("<div id='" + id + "' class='row'>&nbsp;</div>");
+	markup.appendTo("#builds");
 	$.getJSON("http://builds.topicus.local/job/" + name + "/api/json", 
 		function ( data ) {
 			for(var i = 0; i < 5; i++) {
-				var url = data.builds[0].url;
-				getBuildData(url);
+				var url = data.builds[i].url;
+				addBuildData(id, url);
 			}
 		});
 }
 
-function getBuildData(url) {
+function addBuildData(id, url) {
 	$.getJSON(url + "/api/json", function(data) {
-		alert(data.result);
+		if(data.building) 
+		{
+			$("<span class='building'></span>").appendTo('#' + id);
+		}
+		else 
+		{
+			$("<span class='" + data.result.toLowerCase() + "'></span>").appendTo('#' + id);
+		}
 	});
 }
