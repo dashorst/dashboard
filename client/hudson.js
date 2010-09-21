511b83811ef1e385a658a3b45fd29b9dd3171ae7
Martijn Dashorst <martijn.dashorst@gmail.com>
2010-09-21 23:15:22 +0200

    Fixed multi origin problem for hudson builds plugin

--- client/hudson.js
+++ client/hudson.js
@@ -1,32 +1,46 @@
 function getBuildsForProjects() {
-	getBuildsForProject("DUO");
-	getBuildsForProject("EduArte%20v2.03%20-%20trunk%20(ontwikkel)");
-	getBuildsForProject("Vocus%20-%20trunk%20(ontwikkel)");
-	getBuildsForProject("PassePartout");
+    $("#builds").empty().append("<h3>Builds</h3>");
+    getBuildsForProject("DUO");
+    getBuildsForProject("EduArte%20v2.03%20-%20trunk%20(ontwikkel)");
+    getBuildsForProject("Vocus%20-%20trunk%20(ontwikkel)");
+    getBuildsForProject("PassePartout");
 }
 
 function getBuildsForProject(name) {
-	var cleanedName = name.replace(/\(|\)|\.|\-|\%/g, '');
-	var id = 'hudson_' + cleanedName;
-	var markup = $("<div id='" + id + "' class='row'>&nbsp;</div>");
-	$("#builds").empty().append(markup);
-	$.getJSON("http://builds/job/" + name + "/api/json", 
-		function ( data ) {
-			for(var i = 0; i < 5; i++) {
-				var url = data.builds[i].url;
-				addBuildData(id, url);
-			}
-		});
+    var cleanedName = name.replace(/\(|\)|\.|\-|\%/g, '');
+    var id = 'hudson_' + cleanedName;
+    var markup = $("<div id='" + id + "' class='row'>&nbsp;</div>");
+    $("#builds").append(markup);
+    var projectUrl = "http://builds.topicus.local/job/" + name + "/api/json";
+
+    $.ajax({
+        url: projectUrl,
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        success: function(data) {
+            for (var i = 0; i < 5; i++) {
+                var url = data.builds[i].url;
+                addBuildData(id, url);
+            }
+        }
+    });
 }
 
 function addBuildData(id, url) {
-	$.getJSON(url + "/api/json", function(data) {
-		var markup;
-		if(data.building) 
-			markup = $("<span class='building'></span>");
-		else 
-			markup = $("<span class='" + data.result.toLowerCase() + "'></span>");
+    var buildUrl = url + "/api/json";
 
-		markup.appendTo('#' + id)
-	});
+    $.ajax({
+        url: buildUrl,
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        success: function(data) {
+            var markup;
+            if (data.building)
+            	markup = $("<span title='Build #" + data.number + "' class='building'></span>");
+            else
+            	markup = $("<span title='Build #" + data.number + "' class='" + data.result.toLowerCase() + "'></span>");
+
+            markup.appendTo('#' + id)
+        }
+    });
 }
