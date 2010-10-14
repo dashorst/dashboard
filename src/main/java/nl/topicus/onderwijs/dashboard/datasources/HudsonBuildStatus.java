3413f0d015b737af218e8abc9a1cc4144f5bb8fe
Martijn Dashorst <martijn.dashorst@gmail.com>
2010-10-14 10:05:15 +0200

    Added hudson build status datasource and number of unit tests datasource

---
+++ src/main/java/nl/topicus/onderwijs/dashboard/datasources/HudsonBuildStatus.java
@@ -0,0 +1,12 @@
+package nl.topicus.onderwijs.dashboard.datasources;
+
+import java.util.List;
+
+import nl.topicus.onderwijs.dashboard.datatypes.DotColor;
+import nl.topicus.onderwijs.dashboard.modules.DataSource;
+import nl.topicus.onderwijs.dashboard.modules.DataSourceSettings;
+
+@DataSourceSettings(label = "Builds", htmlClass = "build", type = DotColor.class, list = true, conversion = "dot")
+public interface HudsonBuildStatus extends DataSource<List<DotColor>> {
+
+}
