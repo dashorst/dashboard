0816aafab8c6c8bb1ff5252ce2baeeb324403a32
Emond Papegaaij <emond.papegaaij@topicus.nl>
2010-10-19 17:13:32 +0200

    clock

--- src/main/java/nl/topicus/onderwijs/dashboard/web/pages/jquery.dashboardclock.js
+++ src/main/java/nl/topicus/onderwijs/dashboard/web/pages/jquery.dashboardclock.js
@@ -1,29 +1,29 @@
 (function($) {
 	jQuery.fn.dashboardClock = function(starttimeUrl) {
 		var starttime = '';
-		$(document)
-			.data("dashboard-heartbeat-enabled", true)
-			.data("dashboard-heartbeat-count", 0)
-			.everyTime(
-				"1s",
-				"heartbeat-bar-graph",
-				function() {
-					var count = $(document).data("dashboard-heartbeat-count") + 1;
-					if (count % 5 == 0) {
-						$.getJSON(starttimeUrl, function(data){
-							console.log(starttime + " -> "+data);
-							if (starttime == '')
-								starttime = data;
-							if (data && starttime != data) {
-								location.reload(true);
-							}
-						});
+		var doc = $(document);
+
+		doc.data("dashboard-heartbeat-enabled", true);
+		doc.data("dashboard-heartbeat-count", 0);
+		doc.everyTime("1s", "heartbeat-bar-graph", function() {
+			var count = $(document).data("dashboard-heartbeat-count") + 1;
+			if (count % 5 == 0) {
+				$.getJSON(starttimeUrl, function(data) {
+					if (starttime == '')
+						starttime = data;
+					if (data && starttime != data) {
+						location.reload(true);
 					}
-					$(document).data("dashboard-heartbeat-count", count);
-					if ($(document).data("dashboard-heartbeat-enabled"))
-						$(document).triggerHandler("dashboard-heartbeat", count);
 				});
-		$("#stoplink").click(function(){
+			}
+			$(document).data("dashboard-heartbeat-count", count);
+			if ($(document).data("dashboard-heartbeat-enabled"))
+				$(document).triggerHandler("dashboard-heartbeat", count);
+		});
+		doc.everyTime("200ms", "clock-update", function() {
+			$(".clock").text(Date().substring(16, 24));
+		});
+		$("#stoplink").click(function() {
 			var newValue = $(document).data("dashboard-heartbeat-enabled");
 			$(document).data("dashboard-heartbeat-enabled", !newValue);
 		});
